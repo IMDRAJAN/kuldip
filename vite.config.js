4e9072aa2accde461fd3049c018ecb4d54532dc2
RathodRajan <rajanrathod2002md@gmail.com>
2025-11-16 16:43:12 +0530

    update code deploy

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    hmr: {
-      overlay: true,
-    },
-    // allowedHosts: [
-    //   "necessary-screen-delegation-other.trycloudflare.com", // ✅ Add your Cloudflare tunnel domain here
-    // ],
-
-    allowedHosts: true,
-  },
+  // server: {
+  //   hmr: {
+  //     overlay: true,
+  //   },
+  //   allowedHosts: true,
+  // },
 });
+n
