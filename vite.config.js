f84de2a0bc194fb6f1d9428ed180cf7a8e050af5
IMDRAJAN <rp383364@gmail.com>
2025-11-16 17:15:31 +0530

    update solve error

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,14 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/kuldip", // Add this line to set the base path
-  // server: {
-  //   hmr: {
-  //     overlay: true,
-  //   },
-  //   allowedHosts: true,
-  // },
+  server: {
+    hmr: {
+      overlay: true,
+    },
+    // allowedHosts: [
+    //   "necessary-screen-delegation-other.trycloudflare.com", // ✅ Add your Cloudflare tunnel domain here
+    // ],
+
+    allowedHosts: true,
+  },
 });
