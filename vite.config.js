7ca4bf8c6b04717a2c2ccf09aa69e084598c9c19
RathodRajan <rajanrathod2002md@gmail.com>
2025-11-16 16:45:27 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/kuldip/", // Add this line to set the base path
   // server: {
   //   hmr: {
   //     overlay: true,
